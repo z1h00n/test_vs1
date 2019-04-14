1a8a97a4080e0e52d11a5776198db74ee10b7138
Do Ji-Hoon <zihoon9@gmail.com>
2019-04-14 12:57:56 +0900

    add hello world box

--- soulution1/app1/Form1.cs
+++ soulution1/app1/Form1.cs
@@ -16,5 +16,10 @@ namespace app1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello, world!");
+        }
     }
 }
